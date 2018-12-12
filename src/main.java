f9557ade88238e9c6370403e74fd8cecd4dd0882
garrettluskey <garrett.luskey@gmail.com>
2018-12-12 17:37:58 -0600

    Initial Project

---
+++ src/main.java
@@ -0,0 +1,11 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+public class main {
+	public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+	
+	
+	
+}
